7a9661e3a5a8b431678c079b7d93b0480b42d841
Bruce Wang <byyw13@gmail.com>
2017-07-03 10:01:36 -0700

    fix per https://github.com/phoenixframework/phoenix/issues/1953

--- mix.exs
+++ mix.exs
@@ -32,8 +32,8 @@ defmodule Askbywho.Mixfile do
   defp deps do
     [{:phoenix, "~> 1.1.4"},
      {:postgrex, ">= 0.0.0"},
-     {:phoenix_ecto, "~> 2.0"},
-     {:phoenix_html, "~> 2.4"},
+     {:phoenix_ecto, "~> 2.0.1"},
+     {:phoenix_html, "~> 2.6.0"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
      {:cowboy, "~> 1.0"}]
