4c8909e5e96725031605b97f732544acf53ef74c
Ilona Mooney <ilona.mooney@gmail.com>
2017-07-04 14:59:11 +0300

    comma missing

--- config/prod.exs
+++ config/prod.exs
@@ -13,10 +13,9 @@ use Mix.Config
 # which you typically run after static files are built.
 config :askbywho, Askbywho.Endpoint,
   http: [port: {:system, "PORT"}],
-#  url: [host: "example.com", port: 80],
-url: [scheme: "https", host: "askbywho.herokuapp.com", port: 443],
-force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: "askbywho.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 
