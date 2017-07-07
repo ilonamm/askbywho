cca33463a8ac1fcb798c4c893aa07971de102f44
Ilona Mooney <ilona.mooney@gmail.com>
2017-07-07 23:59:36 +0300

    changed back to earlier elixir

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Askbywho.Mixfile do
   def project do
     [app: :askbywho,
      version: "0.0.1",
-     elixir: "~> 1.4.5",
+     elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
