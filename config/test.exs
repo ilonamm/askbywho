f2ec4849d4c6da456e8186715f3e6adf9e92a737
Joao Paulo Barros <joaopaulodesousa@gmail.com>
2017-07-10 14:51:32 -0300

    Change BD pass

--- config/test.exs
+++ config/test.exs
@@ -13,7 +13,7 @@ config :logger, level: :warn
 config :askbywho, Askbywho.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
+  password: "123",
   database: "askbywho_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
