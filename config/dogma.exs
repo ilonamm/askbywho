04ac24786d7dd528c3c068be766c866f7909c94c
Ilona Mooney <ilona.mooney@gmail.com>
2017-07-11 20:43:33 +0300

    change dogma line length rule, actually this time

---
+++ config/dogma.exs
@@ -0,0 +1,13 @@
+# config/dogma.exs
+use Mix.Config
+alias Dogma.Rule
+
+config :dogma,
+
+  # Select a set of rules as a base
+  rule_set: Dogma.RuleSet.All,
+
+  # Override an existing rule configuration
+  override: [
+    %Rule.LineLength{ max_length: 90 },
+  ]
