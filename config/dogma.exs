0f7281ea5529bbd9d16aae7af2662965dfe2ce47
Ilona Mooney <ilona.mooney@gmail.com>
2017-07-11 21:19:28 +0300

    fixing dogma to exclude phoenix and phoenix_html to avoid errors in local environment

--- config/dogma.exs
+++ config/dogma.exs
@@ -10,4 +10,10 @@ config :dogma,
   # Override an existing rule configuration
   override: [
     %Rule.LineLength{ max_length: 125 },
+  ],
+
+  # Pick paths not to lint
+  exclude: [
+    ~r(\Anode_modules/phoenix_html/),
+    ~r(\Anode_modules/phoenix/)
   ]
