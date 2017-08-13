3925f9847404bca724ee319a70c97925be87f10e
Ilona Mooney <ilona.mooney@gmail.com>
2017-08-13 20:58:47 +0300

    finished finnish translation, a few other fixes

--- web/controllers/locale.ex
+++ web/controllers/locale.ex
@@ -1,14 +1,17 @@
 defmodule Askbywho.Locale do
   import Plug.Conn
 
-
   def init(_opts), do: nil
 
   def call(conn, _opts) do
-    langs = %{"en" => "en", "en-us" => "en", "fi" => "fi", "fi-FI" => "fi",
-    "fi-fi" => "fi", "pt-BR" => "pt-BR", "pt-br" => "pt-BR", "pt" => "pt-BR"}
+    # These languages assign a locale automatically
+    langs = %{"en" => "en", "en-us" => "en", "fi" => "fi",
+    "fi-fi" => "fi"}
 
+    # TODO add when portuguese translation is ready
+    # "pt-br" => "pt-BR", "pt" => "pt-BR"
 
+    # fetching browser's guess of what languages the user prefers
     case get_req_header(conn, "accept-language") do
       [language] ->
         pattern  = :binary.compile_pattern([",", ";"])
@@ -16,18 +19,18 @@ defmodule Askbywho.Locale do
         choose_locale(conn, split_string, langs)
     end
 
-
+    # Test different language versions with URL, for example beta.askbywho.com/?locale=fi
     case conn.params["locale"] || get_session(conn, :locale) do
       nil     -> conn
       locale  ->
         Gettext.put_locale(Askbywho.Gettext, locale)
         conn |> put_session(:locale, locale)
     end
-
   end
 
   defp choose_locale(conn, [head | tail], langs) do
-    case Map.fetch(langs, head) do
+    # Parse accept-language from browser and see if they match our language version
+    case Map.fetch(langs, String.downcase(head)) do
       {:ok, l}  ->
         Gettext.put_locale(Askbywho.Gettext, l)
         conn |> put_session(:locale, l)
